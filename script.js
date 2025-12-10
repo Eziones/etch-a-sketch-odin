c6d1ddf97bd92d559315d9aa2f7f5bb5d1ed46ea
Eziones <romaincastro@gmail.com>
2025-12-10 20:04:19 +0100

    Implemented the change size button.
    With input verification to prevent the user from entering a negative number or above 100.

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const container = document.querySelector('#container')
 const CONTAINER_SIZE = 960
 const DEFAULT_GRID_SIZE = 16
 
+const clearGrid = () => {
+  console.log(Array.from(container.children))
+  Array.from(container.children).forEach(child => {
+    container.removeChild(child)
+  });
+}
+
 const generateGrid = (sideSize) => {
   for (let i = 0; i < sideSize * sideSize; i++) {
     const square = document.createElement('div')
@@ -16,5 +23,15 @@ const generateGrid = (sideSize) => {
     container.appendChild(square)
   }
 }
+
+const sizeButton = document.querySelector('.size-button')
+sizeButton.addEventListener('click', () => {
+  let newSize = Number.parseInt(prompt("Enter a new grid size (1-100)"))
+  while(newSize < 1 || newSize > 100) {
+    newSize = Number.parseInt(prompt("Enter a new grid size (1-100)"))
+  }
+  clearGrid()
+  generateGrid(newSize)
+})
 
 generateGrid(DEFAULT_GRID_SIZE)
