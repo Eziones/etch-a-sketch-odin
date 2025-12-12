9c2b055c84af3930bae9182b0962c6ddf8e5e3b5
Eziones <romaincastro@gmail.com>
2025-12-12 20:46:03 +0100

    Add more opacity on hover.
    With increments of .2 to see changes faster. Refactored some code for a better flexbox use.

--- script.js
+++ script.js
@@ -15,12 +15,24 @@ const generateGrid = (sideSize) => {
     square.setAttribute('class', 'square')
     square.style.width = (CONTAINER_SIZE / sideSize) + "px"
     square.style.height = (CONTAINER_SIZE / sideSize) + "px"
+    square.style.flexGrow = 1;
+    square.style.flexShrink = 1;
+    square.style.flexBasis = 100/sideSize + '%'
+
+    console.log(100/sideSize)
 
     square.addEventListener('mouseover', () => {
-      const red = Math.floor(Math.random() * 255)
-      const green = Math.floor(Math.random() * 255)
-      const blue = Math.floor(Math.random() * 255)
-      square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+      if(!square.style.backgroundColor) {
+        const red = Math.floor(Math.random() * 255)
+        const green = Math.floor(Math.random() * 255)
+        const blue = Math.floor(Math.random() * 255)
+        square.style.opacity = .2
+        square.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+      } else {
+        if(Number.parseFloat(square.style.opacity) < 1) {
+          square.style.opacity = Number.parseFloat(square.style.opacity) + .2
+        }
+      }
     })
 
     container.appendChild(square)
@@ -30,7 +42,7 @@ const generateGrid = (sideSize) => {
 const sizeButton = document.querySelector('.size-button')
 sizeButton.addEventListener('click', () => {
   let newSize = Number.parseInt(prompt("Enter a new grid size (1-100)"))
-  while(newSize < 1 || newSize > 100) {
+  while (newSize < 1 || newSize > 100) {
     newSize = Number.parseInt(prompt("Enter a new grid size (1-100)"))
   }
   clearGrid()
